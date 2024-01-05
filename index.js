d710c9b065c3e5e9cf1032cb622be755c5a04df1
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-01-05 17:54:09 +0500

    🛡️ᑭᖇIᑎᑕᕮ ᗷOT ᗰᗪ🛡️

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ if (fs.existsSync('./auth_info_baileys')) {
   
   app.use("/", async(req, res) => {
 
-  const { default: SuhailWASocket, useMultiFileAuthState, Browsers, delay,DisconnectReason, makeInMemoryStore, } = require("@whiskeysockets/baileys");
+  const { default: PrinceWASocket, useMultiFileAuthState, Browsers, delay,DisconnectReason, makeInMemoryStore, } = require("@whiskeysockets/baileys");
   const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
   async function PRINCE() {
     const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys')
